58e55b48da3fe3692c33ebba7714c96df0dfc95c
Masaki Kawabata Neto <masaki.kawabata@gmail.com>
2019-10-03 09:13:07 -0300

    syntax error

--- www/application/libraries/passwordchecker_pam.php
+++ www/application/libraries/passwordchecker_pam.php
@@ -1,22 +1,27 @@
-<?php if (!defined('BASEPATH')) exit('No direct script access allowed'); ?><?php
+<?php if (!defined('BASEPATH')) {
+    exit('No direct script access allowed');
+}
+?><?php
 
 /** Using PAM for password checking. Contributed by questmaster on the Aigaion forums. Requires php_pam_auth module */
-class Passwordchecker_pam {
-    /** 
-    Check password using PAM;
-    */
-    function checkPassword($uname, $password,$pwdInMd5) {
+class Passwordchecker_pam
+{
+    /**
+     * Check password using PAM;
+     */
+    public function checkPassword($uname, $password, $pwdInMd5)
+    {
         if ($pwdInMd5) {
-            return array('uname'=>'','notice'=>sprintf(__('The %s password checker cannot handle md5 passwords'),'PAM'));
+            return array('uname' => '', 'notice' => sprintf(__('The %s password checker cannot handle md5 passwords'), 'PAM'));
         }
 
         //now try to login from PAM
-        if (pam_auth($uname,$password, &$error)) {
-            return array('uname'=>$uname);
+        //if (pam_auth($uname,$password, &$error)) { //causing error
+        if (pam_auth($uname, $password, $error)) {
+            return array('uname' => $uname);
         } else {
             appendErrorMessage($error);
-            return array('uname'=>'');
+            return array('uname' => '');
         }
     }
 }
-?>
